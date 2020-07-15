292507a267c833747887fef5efcf9730c2001d66
shariful-islam-sharif <imdshariful171@gmail.com>
2020-07-16 01:50:34 +0600

    Routing

---
+++ app.js
@@ -0,0 +1,43 @@
+// const EventEmmiter = require("events");
+// const emitter = new EventEmmiter();
+
+// // //Resgister a Listener
+// // emitter.on("messageLogged", function () {
+// //   console.log("Listener Called.");
+// // });
+// // //Raise an Event
+// // emitter.emit("messageLogged");
+
+// const loggerModule = require("./logger");
+// loggerModule.log();
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
